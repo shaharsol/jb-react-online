ea85fb47dd2057666f444b0693227eb0df74d4b8
shaharsol <shahar@gnift.com>
2025-03-13 23:13:04 +0200

    cleanup

--- backend/web/src/routers/feed.ts
+++ backend/web/src/routers/feed.ts
@@ -1,7 +1,4 @@
 import { Router } from "express";
-import { create, getOne, getPerUser, remove, update } from "../controllers/posts/controller";
-import validation from "../middlewares/validation";
-import { editPostValidator, newPostValidator } from "../controllers/posts/validator";
 import { getUserFeed } from "../controllers/feed/controller";
 
 const router = Router();
