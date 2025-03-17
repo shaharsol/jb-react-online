9b2332b4cbfe4921ae049a0853c5f2ee7e77415c
shaharsol <shahar@gnift.com>
2025-03-17 12:06:11 +0200

    navigating

--- 07-routing/src/components/layout/header/Header.tsx
+++ 07-routing/src/components/layout/header/Header.tsx
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 export default function Header() {
     return (
         <div className='Header'>
-            header
+            <div>
+                Logo
+            </div>
+            <div>
+                <nav>
+                    <Link to="/profile">profile</Link>
+                    <Link to="/feed">feed</Link>
+                </nav>
+            </div>
+            <div>
+                Hello username
+            </div>
         </div>
     )
 }
