f918fcd9167e7f275808998f35ff451b5e245b53
shaharsol <shahar@gnift.com>
2025-03-17 11:58:26 +0200

    404

--- 07-routing/src/components/layout/main/Main.tsx
+++ 07-routing/src/components/layout/main/Main.tsx
@@ -1,12 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Profile from "../../posts/profile/Profile";
 import Feed from "../../posts/feed/Feed";
+import NotFound from "../not-found/NotFound";
 
 export default function Main() {
     return (
         <Routes>
+            <Route path="/" element={<Navigate to="/profile"/>} />    
             <Route path="/profile" element={<Profile />} />
             <Route path="/feed" element={<Feed />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
