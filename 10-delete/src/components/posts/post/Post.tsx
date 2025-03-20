f9383b900f838d7ceee5b85289865f480efa45bf
shaharsol <shahar@gnift.com>
2025-03-20 09:59:24 +0200

    deleteMe

--- 10-delete/src/components/posts/post/Post.tsx
+++ 10-delete/src/components/posts/post/Post.tsx
@@ -1,4 +1,5 @@
 import PostModel from '../../../models/post/Post'
+import profileService from '../../../services/profile';
 import './Post.css'
 
 interface PostProps {
@@ -7,15 +8,23 @@ interface PostProps {
 }
 export default function Post(props: PostProps) {
 
-    const { title, body, user, createdAt} = props.post;
+    const { id, title, body, user, createdAt} = props.post;
     const { isAllowedActions } = props;
+
+    async function deleteMe() {
+        try {
+            await profileService.remove(id)
+        } catch (e) {
+            alert(e)
+        }
+    }
 
     return (
         <div className='Post'>
             <div className="title">{title}</div>
             <div className='by-line'>by {user.name} at {(new Date(createdAt)).toLocaleDateString()}</div>
             <div className='body'>{body}</div>
-            {isAllowedActions && <div><button>Delete</button></div>}
+            {isAllowedActions && <div><button onClick={deleteMe}>Delete</button></div>}
             
         </div>
     )
