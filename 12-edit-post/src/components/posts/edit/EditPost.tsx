547bd5b5f4823ca1523fb6d78912ca94780c7ba9
shaharsol <shahar@gnift.com>
2025-03-24 11:43:20 +0200

    edit works

--- 12-edit-post/src/components/posts/edit/EditPost.tsx
+++ 12-edit-post/src/components/posts/edit/EditPost.tsx
@@ -1,27 +1,68 @@
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import './EditPost.css'
 import { useEffect } from 'react'
 import profileService from '../../../services/profile'
+import PostDraft from '../../../models/post/PostDraft'
+import { useForm } from 'react-hook-form'
 
 
 export default function EditPost() {
 
     const { id } = useParams<'id'>()
 
+    const { register, handleSubmit, formState, reset } = useForm<PostDraft>()
+
     useEffect(() => {
         (async() => {
             try {
                 const post = await profileService.getPost(id!)
-                console.log(post)
+                const { title, body } = post
+                reset({ title, body })
             } catch (e) {
                 alert(e)
             }
         })()
-    }, [])
+    }, [ id, reset ])
+
+    const navigate = useNavigate()
+
+    async function submit(draft: PostDraft) {
+        try {
+            await profileService.edit(id!, draft)
+            alert('post edited successfuly')
+            navigate('/profile')
+        } catch (e) {
+            alert(e)
+        }
+    }
 
     return (
         <div className='EditPost'>
-            edit 
+            <form onSubmit={handleSubmit(submit)}>
+                <input type="text" placeholder='Title your post...' {...register('title', {
+                    required: {
+                        value: true,
+                        message: 'Title is a required field'
+                    },
+                    minLength: {
+                        value: 10,
+                        message: 'Title must be longer than 10 chars'
+                    }
+                })}/>
+                <span className='form-error'>{formState.errors.title?.message}</span>
+                <textarea placeholder='Your post body...' {...register('body', {
+                    required: {
+                        value: true,
+                        message: 'Body is a required field'
+                    },
+                    minLength: {
+                        value: 20,
+                        message: 'Body must be longer than 20 chars'
+                    }
+                })}></textarea>
+                <span className='form-error'>{formState.errors.body?.message}</span>
+                <button>Edit Post</button>
+            </form> 
         </div>
     )
 }
