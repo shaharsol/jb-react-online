e3b717ef0711980ad1c7d5ceb2227c74f58ece3d
shaharsol <shahar@gnift.com>
2025-03-27 18:45:48 +0200

    alota

--- 17-sockets/src/components/hooks/useService.ts
+++ 17-sockets/src/components/hooks/useService.ts
@@ -1,3 +1,4 @@
+import { SocketDispatcherContext } from './../socket.io/socket-dispatcher/SocketDispatcherContext';
 import { useContext } from "react";
 import { AuthContext } from "../auth/auth/AuthContext";
 import axios, { AxiosInstance } from "axios";
@@ -5,9 +6,11 @@ import AuthAware from "../../services/auth-aware/AuthAware";
 
 export default function useService<T extends AuthAware>(Service: {new(axiosInstance: AxiosInstance): T}): T {
     const { jwt } = useContext(AuthContext)!
+    const { clientId } = useContext(SocketDispatcherContext)
     const axiosInstance = axios.create({
         headers: {
-            Authorization: `Bearer ${jwt}`
+            Authorization: `Bearer ${jwt}`,
+            'x-client-id': clientId
         }
     })
 
--- 17-sockets/src/components/socket.io/socket-dispatcher/SocketDispatcher.tsx
+++ 17-sockets/src/components/socket.io/socket-dispatcher/SocketDispatcher.tsx
@@ -1,7 +1,10 @@
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useAppDispatch } from "../../../redux/hooks";
 import { newPost } from "../../../redux/profileSlice";
+import { v4 } from "uuid";
+import { SocketDispatcherContext } from "./SocketDispatcherContext";
+import useUserId from "../../hooks/useUserId";
 
 export default function SocketDispatcher(props: PropsWithChildren) {
 
@@ -9,13 +12,20 @@ export default function SocketDispatcher(props: PropsWithChildren) {
 
     const dispatch = useAppDispatch()
 
+    const [ clientId ] = useState<string>(v4())
+
+    const userId = useUserId()
+
     useEffect(() => {
         const socket = io(import.meta.env.VITE_SOCKET_SERVER_URL) 
 
         socket.onAny((eventName, payload) => {
+            if(payload.from === clientId) return;
             switch(eventName) {
                 case 'new-post':
-                    dispatch(newPost(payload.post))
+                    if (userId === payload.post.userId) {
+                        dispatch(newPost(payload.post))
+                    }
                     break;
             }
         })
@@ -23,11 +33,11 @@ export default function SocketDispatcher(props: PropsWithChildren) {
         return () => {
             socket.disconnect()
         }
-    }, [])
+    }, [ dispatch ])
 
     return (
-        <>
+        <SocketDispatcherContext value={{clientId}}>
             {children}
-        </>
+        </SocketDispatcherContext>
     )
 }
