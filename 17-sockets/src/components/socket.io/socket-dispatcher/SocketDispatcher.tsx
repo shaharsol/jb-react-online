c944c6ddbc6eb4c1b1b31b38afa6719b81607190
shaharsol <shahar@gnift.com>
2025-03-27 18:14:34 +0200

    dispacthing

--- 17-sockets/src/components/socket.io/socket-dispatcher/SocketDispatcher.tsx
+++ 17-sockets/src/components/socket.io/socket-dispatcher/SocketDispatcher.tsx
@@ -1,16 +1,23 @@
 import { PropsWithChildren, useEffect } from "react";
 import { io } from "socket.io-client";
+import { useAppDispatch } from "../../../redux/hooks";
+import { newPost } from "../../../redux/profileSlice";
 
 export default function SocketDispatcher(props: PropsWithChildren) {
 
     const { children } = props
 
+    const dispatch = useAppDispatch()
+
     useEffect(() => {
         const socket = io(import.meta.env.VITE_SOCKET_SERVER_URL) 
 
         socket.onAny((eventName, payload) => {
-            console.log(eventName)
-            console.log(payload)
+            switch(eventName) {
+                case 'new-post':
+                    dispatch(newPost(payload.post))
+                    break;
+            }
         })
 
         return () => {
